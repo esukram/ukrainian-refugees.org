40f45e295f90c008e890a4fcf1d8f1f314fd4f59
Markus Heimbach <markus@esukram.org>
2022-02-28 19:37:59 +0100

    Add basic Tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class", //media for browser setting
+};
